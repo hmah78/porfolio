ef825f28cdfe000e262eaea4df09dfb60ebf9ae0
HM Awais Hassan <22100249@lums.edu.pk>
2020-07-12 05:28:31 +0500

    code version 2

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import blog
 
 # Create your views here.
+
+def allblogs(request):
+    blo = blog.objects
+    return render(request,'blog/allblogs.html',{'blo':blo})
+
+def detail(request,blog_id):
+    detailblog= get_object_or_404(blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':detailblog})
